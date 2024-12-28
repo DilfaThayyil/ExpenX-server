ae6ecb75bf0c38cc1194b54a62c82f14c40433c6
DilfaThayyil <dilfathayyil@gmail.com>
2024-12-28 14:04:09 +0530

    validator file added

---
+++ src/utils/validator.ts
@@ -0,0 +1,10 @@
+export const isValidateEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  
+  export const isValidatePassword = (password: string): boolean => {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    return passwordRegex.test(password);
+  };
+  
