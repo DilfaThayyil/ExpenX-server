5fe3867c4dd86031083742ac1faa33aa7362ade9
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-01 23:47:00 +0530

    env setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,8 @@
 require('dotenv').config()
 
 export const MONGODBURL = process.env.MONGODBURL
+export const REDISURL = process.env.REDISURL
+export const NODEMAILEREMAIL = process.env.NODEMAILEREMAIL
+export const NODEMAILERPASSWORD = process.env.NODEMAILERPASSWORD
+export const CLIENTURL = process.env.CLIENTURL
+export const PORT = process.env.PORT
