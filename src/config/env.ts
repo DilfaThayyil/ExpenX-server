ff79f3ed06d4536dcf551a63fe03408e062371ce
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-04 23:34:38 +0530

    env updation

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,3 +7,4 @@ export const NODEMAILERPASSWORD = process.env.NODEMAILERPASSWORD
 export const CLIENTURL = process.env.CLIENTURL
 export const PORT = process.env.PORT
 export const JWT_SECRET = process.env.JWT_SECRET
+export const GOOGLECLIENTID = process.env.GOOGLECLIENTID
