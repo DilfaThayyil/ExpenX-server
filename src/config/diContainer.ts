05a91b18a2ff9e6c3be4375181654a554a0f1885
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-07 18:12:33 +0530

    perf: dependancy injection set for advisor flow

--- src/config/diContainer.ts
+++ src/config/diContainer.ts
@@ -5,7 +5,17 @@ import { IUserService } from '../services/Interface/IUserService';
 import UserService from '../services/Implementation/userService';
 import { IUserController } from '../controllers/Interface/IUserController';
 import UserController from '../controllers/Implementation/userController';
+import { IAdvisorController } from '../controllers/Interface/IAdvisorController';
+import AdvisorController from '../controllers/Implementation/advisorController';
+import { IAdvisorRepository } from '../repositories/Interface/IAdvisorRepository';
+import AdvisorRepository from '../repositories/Implementation/advisorRepository';
+import { IAdvisorService } from '../services/Interface/IAdvisorService';
+import AdvisorService from '../services/Implementation/advisorService';
 
+
+container.register<IAdvisorService>('IAdvisorService',{useClass:AdvisorService})
+container.register<IAdvisorRepository>('IAdvisorRepository',{useClass:AdvisorRepository})
+container.register<IAdvisorController>('IAdvisorController', {useClass: AdvisorController})
 container.register<IUserController>("IUserController", { useClass: UserController });
 container.register<IUserRepository>('IUserRepository', {useClass:UserRepository});
 container.register<IUserService>('IUserService', {useClass:UserService});
