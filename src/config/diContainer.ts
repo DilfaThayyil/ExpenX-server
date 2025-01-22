dfef6c610235f41307751ea65bd8e05fc3583cef
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-22 12:17:31 +0530

    fix: dependancy injection bugs cleared

--- src/config/diContainer.ts
+++ src/config/diContainer.ts
@@ -1,21 +1,37 @@
 import { container } from 'tsyringe';
+
+//user
+import { IAuthUserController } from '../controllers/Interface/user/IAuthUserController';
+import AuthUserController from '../controllers/Implementation/user/authUserController';
+import { IAuthUserService } from '../services/Interface/user/IAuthuserService';
+import AuthUserService from '../services/Implementation/user/authUserService';
 import { IUserRepository } from '../repositories/Interface/IUserRepository';
 import UserRepository from '../repositories/Implementation/userRepository';
-import { IUserService } from '../services/Interface/IUserService';
-import UserService from '../services/Implementation/userService';
-import { IUserController } from '../controllers/Interface/IUserController';
-import UserController from '../controllers/Implementation/userController';
-import { IAdvisorController } from '../controllers/Interface/IAdvisorController';
-import AdvisorController from '../controllers/Implementation/advisorController';
+import { IUserController } from '../controllers/Interface/user/IUserController';
+import UserController from '../controllers/Implementation/user/userController';
+import { IUserService } from '../services/Interface/user/IUserService';
+import UserService from '../services/Implementation/user/userService';
+
+//advisor
+import { IAdvisorController } from '../controllers/Interface/advisor/IAuthAdvisorController';
+import AdvisorController from '../controllers/Implementation/advisor/authAdvisorController';
 import { IAdvisorRepository } from '../repositories/Interface/IAdvisorRepository';
 import AdvisorRepository from '../repositories/Implementation/advisorRepository';
 import { IAdvisorService } from '../services/Interface/IAdvisorService';
 import AdvisorService from '../services/Implementation/advisorService';
 
 
+
+
+
+//user
+container.register<IAuthUserController>("IAuthUserController", { useClass: AuthUserController });
+container.register<IAuthUserService>('IAuthUserService', {useClass:AuthUserService});
+container.register<IUserRepository>('IUserRepository', {useClass:UserRepository});
+container.register<IUserController>("IUserController",{useClass:UserController})
+container.register<IUserService>("IUserService",{useClass:UserService})
+
+//advisor
+container.register<IAdvisorController>('IAdvisorController', {useClass: AdvisorController})
 container.register<IAdvisorService>('IAdvisorService',{useClass:AdvisorService})
 container.register<IAdvisorRepository>('IAdvisorRepository',{useClass:AdvisorRepository})
-container.register<IAdvisorController>('IAdvisorController', {useClass: AdvisorController})
-container.register<IUserController>("IUserController", { useClass: UserController });
-container.register<IUserRepository>('IUserRepository', {useClass:UserRepository});
-container.register<IUserService>('IUserService', {useClass:UserService});
