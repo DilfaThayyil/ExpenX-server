1c537bf3a805e2da3b70e289e4b9abb82632bf3f
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-24 16:16:38 +0530

    feat: added user router for getting and adding expense

--- src/routes/user/userRouter.ts
+++ src/routes/user/userRouter.ts
@@ -4,16 +4,13 @@ import { container } from 'tsyringe';
 import { IUserController } from '../../controllers/Interface/user/IUserController';
 
 const userController = container.resolve<IUserController>('IUserController');
-const router = Router();
+const router = Router()
 
-router.post('/upload', upload.single('profilePic'), (req, res) =>{
-  console.log("req file : ",req.file)
-  console.log("req body : ",req.body)
-  userController.uploadProfileImage(req, res)
-});
 
-router.patch('/editProfile', (req, res) =>
-  userController.updateUser(req, res)
-);
+router.post('/upload', upload.single('profilePic'), (req, res) =>{userController.uploadProfileImage(req, res)})
+router.patch('/editProfile', (req, res) =>userController.updateUser(req, res))
+router.get('/getExpenses/:userId', (req,res)=>userController.getExpenses(req,res))
+router.post('/createExpense/:userId',(req,res)=>userController.createExpense(req,res))
+
 
 export default router
