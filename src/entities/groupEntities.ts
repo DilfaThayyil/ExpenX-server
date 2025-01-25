a866f105d4e1f8766f1ee3907cf31d7da821e631
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-25 11:16:32 +0530

    fix(group): resolve issues with Group schema and IGroup interface

--- src/entities/groupEntities.ts
+++ src/entities/groupEntities.ts
@@ -1,10 +1,17 @@
-import IUser from "./userEntities"
+// import IUser from "./userEntities"
 
-export default interface IGroup{
-    name: string
-    description: string
-    members: IUser[]
-    createdBy: string
-    createdAt: Date
-    updatedAt: Date
-}
+// export default interface IGroup{
+//     name: string
+//     description: string
+//     members: IUser[]
+//     createdBy: string
+//     createdAt: Date
+//     updatedAt: Date
+// }
+
+export default interface IGroup {
+    name: string;
+    members: string[];
+    splitMethod: string;
+  }
+  
--- src/models/groupSchema.ts
+++ src/models/groupSchema.ts
@@ -1,11 +1,36 @@
-import { model, Schema } from "mongoose";
-import IGroup from "../entities/groupEntities";
+// import { model, Schema } from "mongoose";
+// import IGroup from "../entities/groupEntities";
 
-const GroupSchema: Schema = new Schema({
-    name: {type:String},
-    description: {type:String},
-    members: [{type: Schema.Types.ObjectId,ref: 'User'}],
-    createdAt: {type:Date, default:Date.now},
-    updatedAt: {type:Date, default:Date.now}
-})
-export default model<IGroup>('Group',GroupSchema)
+// const GroupSchema: Schema = new Schema(
+//     {
+//         name: { type: String },
+//         members: [{ type:String}],
+//         expenses: [{ type: String }],
+//         splitMethod: { type: String },
+//         createdBy: {type:String}
+//     },
+//     { timestamps: true }
+// )
+// export default model<IGroup>('Group', GroupSchema)
+
+
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IGroup extends Document {
+  name: string;
+  members: string[];
+  splitMethod: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const GroupSchema = new Schema<IGroup>(
+  {
+    name: { type: String},
+    members: { type: [String]},
+    splitMethod: { type: String},
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model<IGroup>('Group', GroupSchema);
