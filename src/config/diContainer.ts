f85c4e33c846370a4a49a88f223e4506c654d780
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-27 05:01:59 +0530

    fix(diContainer): resolve dependency injection registration issue for AdvisorController

--- src/config/diContainer.ts
+++ src/config/diContainer.ts
@@ -13,15 +13,16 @@ import { IUserService } from '../services/Interface/user/IUserService';
 import UserService from '../services/Implementation/user/userService';
 
 //advisor
-import { IAdvisorController } from '../controllers/Interface/advisor/IAuthAdvisorController';
-import AdvisorController from '../controllers/Implementation/advisor/authAdvisorController';
+import { IAuthAdvisorController } from '../controllers/Interface/advisor/IAuthAdvisorController';
+import AuthAdvisorController from '../controllers/Implementation/advisor/authAdvisorController';
+import { IAuthAdvisorService } from '../services/Interface/advisor/IAuthAdvisorService';
+import AuthAdvisorService from '../services/Implementation/advisor/authAdvisorService';
 import { IAdvisorRepository } from '../repositories/Interface/IAdvisorRepository';
 import AdvisorRepository from '../repositories/Implementation/advisorRepository';
-import { IAdvisorService } from '../services/Interface/IAdvisorService';
-import AdvisorService from '../services/Implementation/advisorService';
-
-
-
+import { IAdvisorController } from '../controllers/Interface/advisor/IAdvisorController';
+import AdvisorController from '../controllers/Implementation/advisor/advisorController';
+import { IAdvisorService } from '../services/Interface/advisor/IAdvisorService';
+import AdvisorService from '../services/Implementation/advisor/advisorService';
 
 
 //user
@@ -32,6 +33,8 @@ container.register<IUserController>("IUserController",{useClass:UserController})
 container.register<IUserService>("IUserService",{useClass:UserService})
 
 //advisor
-container.register<IAdvisorController>('IAdvisorController', {useClass: AdvisorController})
-container.register<IAdvisorService>('IAdvisorService',{useClass:AdvisorService})
+container.register<IAuthAdvisorController>('IAuthAdvisorController', {useClass: AuthAdvisorController})
+container.register<IAuthAdvisorService>('IAuthAdvisorService',{useClass:AuthAdvisorService})
 container.register<IAdvisorRepository>('IAdvisorRepository',{useClass:AdvisorRepository})
+container.register<IAdvisorController>('IAdvisorController',{useClass:AdvisorController})
+container.register<IAdvisorService>('IAdvisorService',{useClass:AdvisorService})
