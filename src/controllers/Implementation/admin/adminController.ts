0489ace3c74df7c9ae67116fe4754cf037c46a67
DilfaThayyil <dilfathayyil@gmail.com>
2025-04-14 10:47:20 +0530

    conflict-222

--- src/controllers/Implementation/admin/adminController.ts
+++ src/controllers/Implementation/admin/adminController.ts
@@ -16,7 +16,8 @@ export default class AdminController implements IAdminController {
   private adminService: IAdminService
 
   constructor(@inject('IAdminService') adminService: IAdminService) {
-    this.adminService = adminService
+    console.log('conflict')
+    this.adminService = adminService;
   }
 
 
